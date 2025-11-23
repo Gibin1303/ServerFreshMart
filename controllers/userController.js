309ce4f755e92ad6b8572a6d0c118241d4eebdfb
Gibin <gibin@aventusinformatics.com>
2025-11-23 19:32:38 +0530

    added /

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,6 +99,7 @@ export const logout = async(req,res)=>{
          secure:process.env.NODE_ENV === "production", 
          sameSite:process.env.NODE_ENV === "production" ? 'none' : 'strict', 
          maxAge:7 * 24 * 60 * 60 * 1000,
+         path:"/"
      })
 
      return res.json({success:true,message:"Logged Out Successfully"})
