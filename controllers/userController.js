75ceda43ed53b87cb694436a033b844da156486b
Gibin <gibin@aventusinformatics.com>
2025-11-23 19:21:39 +0530

    cleared bugs

--- controllers/userController.js
+++ controllers/userController.js
@@ -96,7 +96,7 @@ export const logout = async(req,res)=>{
    try{
       res.clearCookie('token',{
          httpOnly:true,    
-         Secure:process.env.NODE_ENV === "production", 
+         secure:process.env.NODE_ENV === "production", 
          sameSite:process.env.NODE_ENV === "production" ? 'none' : 'strict', 
          maxAge:7 * 24 * 60 * 60 * 1000,
      })
